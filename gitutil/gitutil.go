5c8abc6bd2c5174f90227525cd9a5656cc366459
elliotxx <951376975@qq.com>
2021-11-01 14:43:32 +0800

    refactor: improve gitutil pkg

--- gitutil/gitutil.go
+++ gitutil/gitutil.go
@@ -10,7 +10,45 @@ import (
 // https://git-scm.com/docs/git-tag
 // https://github.com/vivin/better-setuptools-git-version/blob/master/better_setuptools_git_version.py
 
-func GetLatestTag() (tag string, err error) {
+// get remote url
+func GetRemoteUrl() (string, error) {
+	stdout, err := exec.Command(
+		"git", "config", "--get", "remote.origin.url",
+	).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(stdout)), nil
+}
+
+func GetLatestTag() (string, error) {
+	tag, err := GetLatestTagFromLocal()
+	if tag == "" || err != nil {
+		return GetLatestTagFromRemote()
+	}
+	return tag, nil
+}
+
+// get latest tag from remote,
+// the fitting git clone depth is 1
+func GetLatestTagFromRemote() (tag string, err error) {
+	// get remote url
+	remoteUrl, err := GetRemoteUrl()
+	if err != nil {
+		return "", err
+	}
+
+	// get latest tag from remote
+	stdout, err := exec.Command(
+		`bash`, `-c`, `git ls-remote --tags --sort=v:refname `+remoteUrl+` | tail -n1 | sed 's/.*\///; s/\^{}//'`,
+	).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(stdout)), nil
+}
+
+func GetLatestTagFromLocal() (tag string, err error) {
 	tags, err := GetTagList()
 	if err != nil {
 		return "", err
@@ -18,7 +56,7 @@ func GetLatestTag() (tag string, err error) {
 	if len(tags) > 0 {
 		tag = tags[len(tags)-1]
 	}
-	return tag, nil
+	return strings.TrimSpace(tag), nil
 }
 
 func GetTagList() (tags []string, err error) {
@@ -67,6 +105,14 @@ func GetTagCommitSha(tag string) (sha string, err error) {
 		return "", fmt.Errorf("gitutil.GetTagCommitSha: empty tag")
 	}
 
+	sha, err = GetTagCommitShaFromLocal(tag)
+	if sha == "" || err != nil {
+		return GetTagCommitShaFromRemote(tag)
+	}
+	return
+}
+
+func GetTagCommitShaFromLocal(tag string) (sha string, err error) {
 	// git rev-list -n 1 {tag}
 	stdout, err := exec.Command(
 		`git`, `rev-list`, `-n`, `1`, tag,
@@ -74,18 +120,34 @@ func GetTagCommitSha(tag string) (sha string, err error) {
 	if err != nil {
 		return "", err
 	}
-
 	var lines []string
 	for _, s := range strings.Split(strings.TrimSpace(string(stdout)), "\n") {
 		if s := strings.TrimSpace(s); s != "" {
 			lines = append(lines, s)
 		}
 	}
-
 	if len(lines) > 0 {
 		sha = lines[len(lines)-1]
 	}
-	return
+	return strings.TrimSpace(sha), nil
+}
+
+// get tag commit sha from remote,
+// the fitting git clone depth is 1
+func GetTagCommitShaFromRemote(tag string) (string, error) {
+	// get remote url
+	remoteUrl, err := GetRemoteUrl()
+	if err != nil {
+		return "", err
+	}
+	// git ls-remote --tags --sort=v:refname git@xxx:xxx/xxx.git | tail -n1 | awk '{print $1}'
+	stdout, err := exec.Command(
+		`bash`, `-c`, `git ls-remote --tags --sort=v:refname `+remoteUrl+` | tail -n1 | awk '{print $1}'`,
+	).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(stdout)), nil
 }
 
 func IsHeadAtTag(tag string) (bool, error) {
