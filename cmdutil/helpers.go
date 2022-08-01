5d5d8d3eb251b10eb7e6facd877fb5af56a5833e
elliotxx <951376975@qq.com>
2022-08-01 11:26:15 +0800

    feat: add cmdutil pkg

---
+++ cmdutil/helpers.go
@@ -0,0 +1,70 @@
+package cmdutil
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/elliotxx/gulu/pretty"
+	"github.com/hashicorp/go-multierror"
+)
+
+const (
+	DefaultErrorExitCode = 1
+)
+
+var fatalErrHandler = fatal
+
+// fatal prints the message (if provided) and then exits.
+func fatal(msg string, code int) {
+	if len(msg) > 0 {
+		// add newline if needed
+		if !strings.HasSuffix(msg, "\n") {
+			msg += "\n"
+		}
+		pretty.ErrorT.WithWriter(os.Stderr).Print(msg)
+	}
+	os.Exit(code)
+}
+
+// ErrExit may be passed to CheckError to instruct it to output nothing but exit with
+// status code 1.
+var ErrExit = fmt.Errorf("exit")
+
+// CheckErr prints a user friendly error to STDERR and exits with a non-zero
+// exit code. Unrecognized errors will be printed with an "error: " prefix.
+//
+// This method is generic to the command in use and may be used by non-Kubectl
+// commands.
+//
+// Example:
+//   func main() {
+//   	cmd := NewRootCommand()
+//
+//   	if err := cmd.Execute(); err != nil {
+//   		cmdutil.CheckErr(err)
+//   	}
+//   }
+func CheckErr(err error) {
+	checkErr(err, fatalErrHandler)
+}
+
+// checkErr formats a given error as a string and calls the passed handleErr
+// func with that string and an kubectl exit code.
+func checkErr(err error, handleErr func(string, int)) {
+	// flatten errors
+	if merr, ok := err.(*multierror.Error); ok {
+		err = multierror.Flatten(merr.ErrorOrNil())
+	}
+
+	if err == nil {
+		return
+	}
+
+	switch {
+	case err == ErrExit:
+		handleErr("", DefaultErrorExitCode)
+	default:
+		handleErr(err.Error(), DefaultErrorExitCode)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.17
 require (
 	bou.ke/monkey v1.0.2
 	github.com/clbanning/mxj/v2 v2.5.6
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/hashicorp/go-version v1.3.0
 	github.com/niklasfasching/go-org v1.6.5
 	github.com/pelletier/go-toml/v2 v2.0.2
 	github.com/pkg/errors v0.9.1
-	github.com/pterm/pterm v0.12.37
+	github.com/pterm/pterm v0.12.45
 	github.com/spf13/afero v1.9.2
 	github.com/spf13/cast v1.5.0
 	github.com/spf13/jwalterweatherman v1.1.0
@@ -20,10 +21,15 @@ require (
 )
 
 require (
+	atomicgo.dev/cursor v0.1.1 // indirect
+	atomicgo.dev/keyboard v0.2.8 // indirect
 	github.com/BurntSushi/toml v0.4.1 // indirect
 	github.com/atomicgo/cursor v0.0.1 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gookit/color v1.4.2 // indirect
+	github.com/gookit/color v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/lithammer/fuzzysearch v1.1.5 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
@@ -31,7 +37,7 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
